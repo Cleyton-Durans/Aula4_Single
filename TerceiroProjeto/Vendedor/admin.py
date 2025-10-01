cc2835d031f2842e3ea08bfa20b2c314233428dc
CLEYTON DURANS DO NASCIMENTO <cleyton.nascimento@aluno.senac.rio>
2025-10-01 20:08:11 -0300

    Durans atualizando

--- TerceiroProjeto/Vendedor/admin.py
+++ TerceiroProjeto/Vendedor/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Vendedor
 
 # Register your models here.
+
+@admin.register(Vendedor)
+
+class VendedorAdmin(admin.ModelAdmin):
+
+    list_display = ('nome', 'email', 'ativo' , 'cpf', 'rg', 'telefone', 'endereco' , 'data_nascimento' , 'sexo', 'ativo', 'criado_em' , 'atualizado_em')
+
+    search_fields = ('nome', 'email')
--- TerceiroProjeto/Vendedor/models.py
+++ TerceiroProjeto/Vendedor/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Vendedor(models.Model):
     nome = models.CharField(max_length=100)
     email = models.EmailField()
-    idade = models.IntegerField()
     ativo = models.BooleanField(default=True)
-
+    cpf = models.CharField()
+    rg = models.CharField()
+    telefone = models.CharField()
+    endereco = models.CharField()
+    data_nascimento =  models.DateField()
+    sexo = models.CharField()
+    criado_em = models.DateTimeField(auto_now_add=True)
+    atualizado_em = models.DateTimeField(auto_now_add=True)
