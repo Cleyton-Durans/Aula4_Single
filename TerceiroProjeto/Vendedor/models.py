7a0aea231fbd24d7920985ece2684f98a3c7a757
CLEYTON DURANS DO NASCIMENTO <cleyton.nascimento@aluno.senac.rio>
2025-10-01 21:25:19 -0300

    Durans

--- TerceiroProjeto/Vendedor/models.py
+++ TerceiroProjeto/Vendedor/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Vendedor(models.Model):
+
+    sexo_choises = (
+        ("F", "Feminino"),
+        ("M", "Masculino"),
+        ("N", "Nenhuma das opções")
+    )
+
     nome = models.CharField(max_length=100)
     email = models.EmailField()
     ativo = models.BooleanField(default=True)
@@ -10,6 +17,6 @@ class Vendedor(models.Model):
     telefone = models.CharField()
     endereco = models.CharField()
     data_nascimento =  models.DateField()
-    sexo = models.CharField()
+    sexo = models.CharField(max_length=1, choices=sexo_choises, blank=False, null=False)
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now_add=True)
--- TerceiroProjeto/Vendedor/views.py
+++ TerceiroProjeto/Vendedor/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponseNotAllowed
+from .models import Vendedor
 
 # Create your views here.
+
+# "request" possui toda a requisição dentro dele.
+def listar_clientes(request):
+    if request.method != "GET":
+        return HttpResponseNotAllowed(["GET"])
+    clientes = Vendedor.objects.all().order_by("nome").values("id","nome","cpf","rg","email","telefone")
+    return JsonResponse(list(clientes), safe=False)
+
